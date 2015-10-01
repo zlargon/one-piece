8ec20df53483bc17d90e1905b794613ffeb94356
zlargon <zlargon@icloud.com>
2015-10-02 04:00:54 +0800

    update script and config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 module.exports = {
     maxCharacterNumber: {
-        jp: 724,
+        jp: 743,
         tw: 267
     },
     path: {
-        characterData: path.resolve(__dirname, "data/character_data.js")
+        characterFolder: path.resolve(__dirname, "data/character")
     }
 };
--- scripts/update_character_data.js
+++ scripts/update_character_data.js
@@ -1,81 +1,48 @@
 var fs        = require("fs");
+var path      = require("path");
 var config    = require("../config.js");
 var character = require("../lib/character.js");
 
-// update character data
-var updateData = function updateData() {
-    var characterList = require(config.path.characterData);
+function updateCharacter(number) {
+    var file = path.resolve(config.path.characterFolder, number + ".js");
 
-    var updateList = [];
-    var getCharacter = function getCharacter(no) {
-
-        // work is done. write the character list back to the file
-        if (no > config.maxCharacterNumber.jp) {
-            var content = "module.exports = " + JSON.stringify(characterList, null, 2) + ";\n";
-            fs.writeFile(config.path.characterData, content, function (err) {
-                if (err !== null) throw err;
-                console.log("Character no." + updateList.join(", ") + " has been updated");
-            });
-            return;
+    function nextStep () {
+        if (number >= config.maxCharacterNumber.jp) {
+            console.log("ALL DONE");
+        } else {
+            updateCharacter(number + 1);
         }
-
-        // character data is already exist
-        if (characterList[no] !== null && characterList[no].name.jp !== null) {
-            getCharacter(no + 1);
-            return;
-        }
-
-        // fetch character no.XX
-        character.fetch({ from: no, to: no }, function (num, err, list) {
-            if (err !== null) {
-                console.error(err);
-            } else {
-                updateList.push(no);
-                characterList[no] = list[no];
-            }
-
-            getCharacter(no + 1);
-        });
     };
 
-    // start fetching character data
-    getCharacter(1);
-}
+    fs.stat(file, function(err, stat) {
+        // 1. file is exist, update the data
+        if (err === null) {
+            nextStep();
+            return;
+        }
 
-// check whether the file is exist
-fs.stat(config.path.characterData, function(err, stat) {
-
-    // 1. file is exist, update the data
-    if (err === null) {
-        updateData();
-        return;
-    }
-
-    // 2. file is exist, but read file error
-    if (err.code !== "ENOENT") {
-        console.error(err);
-        return;
-    }
-
-    // 3. file is not exist. fetch all the character data, and write into the file
-    character.fetch({ from: 1, to: config.maxCharacterNumber.jp }, function (num, err, list) {
-        // fetch character data error
-        if (err !== null) {
+        // 2. file is exist, but read file error
+        if (err.code !== "ENOENT") {
             console.error(err);
             return;
         }
 
-        // write into the file
-        var content = "module.exports = " + JSON.stringify(list, null, 2) + ";\n";
-        fs.writeFile(config.path.characterData, content, function (err) {
-            // write file error
+        // 3. file is not exist. fetch all the character data, and write into the file
+        character.fetch(number, function(no, err, character) {
             if (err !== null) {
-                console.error(err);
+                console.log(err);
+                nextStep();
                 return;
             }
 
-            // done
-            console.log("Character no." + num.from + " ~ " + num.to + " has been written into " + config.path.characterData);
+            var content = "module.exports = " + JSON.stringify(character, null, 2) + ";\n";
+            fs.writeFile(file, content, function (err) {
+                if (err !== null) throw err;
+                nextStep();
+            });
         });
     });
-});
+}
+
+// start from character no.1
+updateCharacter(1);
