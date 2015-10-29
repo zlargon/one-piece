0cf74e4b85dfe1250fc2ef05ebec18acba4e498e
zlargon <zlargon@icloud.com>
2015-10-30 02:38:33 +0800

    [config] add node module 'react' and let babel-loader support ES7 syntax
    rename main.js to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 var path = require('path');
 
 module.exports = {
-    entry: [path.resolve(__dirname, 'app/main.js')],
+    entry: [
+        path.resolve(__dirname, 'app/index.js')
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            loaders: ['babel']
-        }]
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: path.join(__dirname, 'node_modules'),
+                loader: 'babel-loader',
+                query: { stage: 0 }     // support ES7 syntax
+            }
+        ]
     }
 };
