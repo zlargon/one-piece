b52fef2eb97bb5dd1e77294feedcfc17f431222c
zlargon <zlargon@icloud.com>
2015-10-30 02:40:25 +0800

    [react] create OnePiece React Component Class

---
+++ app/OnePiece.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class OnePiece extends React.Component {
+    render () {
+        return (
+            <div>OnePiece</div>
+        );
+    }
+}
--- app/index.js
+++ app/index.js
@@ -1,4 +1,10 @@
-var OnePiece = require("one-piece");
+import OnePiece from 'one-piece';
+import React    from 'react';
+import ReactDOM from 'react-dom';
+import App      from './OnePiece';
 
 // set OnePiece as Global variable
 window.OnePiece = OnePiece;
+
+// Start React
+ReactDOM.render(<App />, document.getElementById('root'));
