dedd2a2d86d57e00979f49bf92f89166b34a0fa0
zlargon <zlargon@icloud.com>
2015-11-05 13:13:27 +0800

    [UI] show analysis in UI level

--- app/OnePiece.js
+++ app/OnePiece.js
@@ -51,21 +51,11 @@ export default class OnePiece extends React.Component {
     }
 
     componentWillUpdate(nextProps, nextState) {
-        const { enemy, boat, characters } = nextState;
-        const team = characters.map(character => {
-            return {
-                no: character.no,
-                attack: character.attack,
-                bead: character.bead,
-                timing: character.timing,
-                captain: character.captainEffect
-            };
-        });
+        const data = nextState;
+        console.log(analysisToString(attackAnalysis(data)));
 
         // save the data to local storage
-        window.localStorage.setItem('data', JSON.stringify(nextState));
-
-        attackAnalysis({ enemy, boat, team });
+        window.localStorage.setItem('data', JSON.stringify(data));
     }
 
     render () {
@@ -89,4 +79,42 @@ export default class OnePiece extends React.Component {
             </div>
         );
     }
+}
+
+function analysisToString(data) {
+  const { captains, analysis } = data;
+
+  // Captain Effect
+  const captainEffectContent = captains.reduce((text, character, index) => {
+    const name = character.name.tw ? character.name.tw : character.name.jp;
+    const content = character.captainEffect.tw.content ? character.captainEffect.tw.content : character.captainEffect.jp.content;
+    return text + `\n${index + 1}. ${name} No.${character.no}\n   ${content}\n`;
+  }, '');
+
+  // Analysis
+  const analysisContent = analysis.reduce((text, { character, magnification: magni, attack, damage, total }, index) => {
+    return text +
+`\n第 ${index + 1} 位：${character.name.tw || character.name.jp}
+
+原攻擊力 = ${attack.original}, 連擊 = ${character.combo}
+梅莉號：${magni.boat}, 船長：${magni.captain}, 屬珠：${magni.bead}, 剋屬：${magni.type}, Chain：${magni.chain}
+加成後攻擊力 = ${attack.basic}
+單擊攻擊力   = ${attack.singal}
+
+前 ${attack.combo - 1} 擊傷害  = ${damage.singal}
+最後一擊傷害 = ${damage.final} (${attack.timing.toUpperCase()})
+
+累計傷害：
+${damage.history.join('\n')} => ${damage.total}
+
+Combo = ${total.combo}
+Total = ${total.attack}
+
+--------------------------------------------------------\n`
+  }, '');
+
+  return '船長效果：\n'
+    + captainEffectContent
+    + '\n--------------------------------------------------------\n'
+    + analysisContent;
 }
