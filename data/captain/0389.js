bff1403102379391c885cbd330067fb51ff9304e
zlargon <zlargon@icloud.com>
2015-11-09 16:26:06 +0800

    [feature] support captain effect no.389, 390

---
+++ data/captain/0389.js
@@ -0,0 +1,30 @@
+import Character from '../../lib/CharacterClass';
+
+export default function ({ characterList }) {
+  const TypeOrder = [ '技', '知', '速' ];
+  let index = 0;
+
+  for (let i = 0; i < characterList.length; i++) {
+    const character = characterList[i];
+    const characterType = Character.get(character.no).type;
+    const nextType = TypeOrder[index];
+
+    if (characterType !== nextType || !isAboveGood(character.timing)) {
+      index = 0;  // restart index
+      continue;
+    }
+
+    // is not the last type
+    if (index !== TypeOrder.length - 1) {
+      index++;
+      continue;
+    }
+
+    return 2.25;
+  }
+  return 1;
+}
+
+function isAboveGood(timing) {
+  return timing === 'good' || timing === 'great' || timing === 'perfect';
+}
